8058679adca2d7a6faab17e30eee660e1b8f0854
Lars Willrich <lars-oliver.willrich@fokus.fraunhofer.de>
2014-08-12 15:11:18 +0200

    example Notification

--- panel-ui/src/main/java/de/fhg/fokus/streetlife/mmecp/server/service/EventInfoServiceImpl.java
+++ panel-ui/src/main/java/de/fhg/fokus/streetlife/mmecp/server/service/EventInfoServiceImpl.java
@@ -4,8 +4,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
@@ -25,85 +29,48 @@ public class EventInfoServiceImpl extends RemoteServiceServlet implements
 
 	}
 
-	int eventID = 1;
+	private static int eventID = 1;
+	private static ArrayList<EventInfo> eventInfoQueue = new ArrayList<EventInfo>();
+	private static JSONProcessor jSONProcessor = null;
+	private static ServletContext context = null;
 
 	public EventInfo getEventInfo() {
-		try {
-			Thread.sleep(500);
-		} catch (InterruptedException e1) {
-			e1.printStackTrace();
+
+		context = getServletContext();
+
+		// get JSON schema
+		if (jSONProcessor == null) {
+			try {
+				jSONProcessor = new JSONProcessor(
+						getContentOfFile("/WEB-INF/schema.json"));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 
-		EventInfo eventInfo = new EventInfo();
-		if (eventID % 2 == 0)
+		EventInfo eventInfo = null;
+		while ((eventInfoQueue.size() == 0)) {
 			try {
-				Thread.sleep(1000 * 10);
+				Thread.sleep(500);
 			} catch (InterruptedException e1) {
 				e1.printStackTrace();
 			}
-		
-		if (eventID % 8 == 0)
-			try {
-				Thread.sleep(10000 * 10);
-			} catch (InterruptedException e1) {
-				e1.printStackTrace();
-			}
-		
-		//get JSON schema
-		JSONProcessor jSONProcessor = null;
-		try {
-			jSONProcessor = new JSONProcessor(
-					getContentOfFile("/WEB-INF/schema.json"));
-		} catch (IOException e) {
-			e.printStackTrace();
-			eventInfo.setMessage(e.getMessage());
-			System.out.println(eventInfo.getMessage());
-			return eventInfo;
 		}
 
-		
-		//get JSON example
-		String jSONExample = "";
-		try {
-			// Example at first
-			jSONExample = getJSONExample();
-		} catch (IOException e) {
-			e.printStackTrace();
-			eventInfo.setMessage(e.getMessage());
-			System.out.println(eventInfo.getMessage());
-			return eventInfo;
-		}
+		// Not consider multiple clients! TODO!
+		eventInfo = eventInfoQueue.remove(eventInfoQueue.size() - 1);
 
-		//validation
-		if (!jSONProcessor.validate(jSONExample)) {
-			eventInfo.setMessage("json file not valid");
-			System.out.println(eventInfo.getMessage());
-			return eventInfo;
-		}
-
-		//Parse
-		try {
-			eventInfo = JSONProcessor.parse(jSONExample);
-		} catch (Exception e) {
-			eventInfo.setMessage(e.getMessage());
-			System.out.println(eventInfo.getMessage());
-			return eventInfo;
-		}
-		eventInfo.setMessage("success");
-		System.out.println(eventInfo.getMessage());
-		eventInfo.setId(eventID++);
 		return eventInfo;
 	}
 
-	private String getJSONExample() throws IOException {
+	private static String getJSONExample() throws IOException {
 		String filename = "/WEB-INF/example.json";
 		return getContentOfFile(filename);
 	}
 
-	private String getContentOfFile(String path) throws IOException {
+	private static String getContentOfFile(String path) throws IOException {
 		String string = "";
 
-		ServletContext context = getServletContext();
 		InputStream inp = context.getResourceAsStream(path);
 		if (inp != null) {
 			InputStreamReader isr = new InputStreamReader(inp);
@@ -114,5 +81,50 @@ public class EventInfoServiceImpl extends RemoteServiceServlet implements
 			}
 		}
 		return string;
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		if (req.getServletPath().compareTo("/exampleData") == 0) {
+			System.out.println("doGET!");
+			eventID = 1;
+		}
+	}
+
+	public static void newEvent() {
+		EventInfo eventInfo = new EventInfo();
+		// get JSON example
+		String jSONExample = "";
+		try {
+			// Example at first
+			jSONExample = getJSONExample();
+		} catch (IOException e) {
+			e.printStackTrace();
+			eventInfo.setMessage(e.getMessage());
+			System.out.println(eventInfo.getMessage());
+		}
+
+		// validation
+		if (!jSONProcessor.validate(jSONExample)) {
+			eventInfo.setMessage("json file not valid");
+			System.out.println(eventInfo.getMessage());
+		}
+
+		// Parse
+		try {
+			eventInfo = JSONProcessor.parse(jSONExample);
+		} catch (Exception e) {
+			eventInfo.setMessage(e.getMessage());
+			System.out.println(eventInfo.getMessage());
+		}
+		eventInfo.setMessage("success");
+		System.out.println(eventInfo.getMessage());
+		eventInfo.setId(eventID++);
+
+		eventInfoQueue.add(eventInfo);
+
+		System.out.println("New Event " + eventID + " added");
 	}
 }

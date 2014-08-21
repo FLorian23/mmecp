845734286f0b9527e34791c59c203ebaf50d1b92
email <benjamin.dittwald@fokus.fraunhofer.de>
2014-08-21 14:43:06 +0200

    edit

--- data-aggregator/src/main/java/de/fhg/fokus/streetlife/mmecp/dataaggregator/DataAggregatorClientImpl.java
+++ data-aggregator/src/main/java/de/fhg/fokus/streetlife/mmecp/dataaggregator/DataAggregatorClientImpl.java
@@ -1,31 +1,28 @@
 package de.fhg.fokus.streetlife.mmecp.dataaggregator;
 
-import de.fhg.fokus.streetlife.mmecp.dataaggregator.restclient.AtomClient;
-import de.fhg.fokus.streetlife.mmecp.dataaggregator.restclient.AtomClientImpl;
+import java.util.Properties;
+
 import org.jboss.resteasy.spi.NotImplementedYetException;
 
-import java.util.Properties;
+import de.fhg.fokus.streetlife.mmecp.dataaggregator.restclient.AtomClientImpl;
 
 /**
  * Created by benjamin on 20.08.14.
  */
 public class DataAggregatorClientImpl implements DataAggregatorClient {
 
-    @Override
-    public void init() {
-        throw new NotImplementedYetException();
-    }
+	public void init() {
+		throw new NotImplementedYetException();
+	}
 
-    @Override
-    public void init(Properties props) {
-        throw new NotImplementedYetException();
-    }
+	public void init(Properties props) {
+		throw new NotImplementedYetException();
+	}
 
-    @Override
-    public String getNotifications(String channelId) {
+	public String getNotifications(String channelId) {
 
-        AtomClientImpl ac = new AtomClientImpl();
+		AtomClientImpl ac = new AtomClientImpl();
 
-        return ac.getNotification(channelId);
-    }
+		return ac.getNotification(channelId);
+	}
 }

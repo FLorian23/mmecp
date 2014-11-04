f719f62fcaba95aa33be4de82459407280372e53
email <benjamin.dittwald@fokus.fraunhofer.de>
2014-11-04 08:09:07 +0100

    test fix

---
+++ configurator/src/test/java/de/fhg/fokus/streetlife/configurator/ConfiguratorTest.java
@@ -0,0 +1,15 @@
+package de.fhg.fokus.streetlife.configurator;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by bdi on 04/11/14.
+ */
+public class ConfiguratorTest {
+
+    @Test
+    public void trueTest() {
+        Assert.assertTrue(true);
+    }
+}

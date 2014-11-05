4db0687f2afe0df66fe4edd52b382965c65dc6cd
email <benjamin.dittwald@fokus.fraunhofer.de>
2014-11-05 15:53:27 +0100

    ediz

--- api-websocket/src/main/java/de/fhg/fokus/streetlife/mmecp/websocket/ToPanelEndpoint.java
+++ api-websocket/src/main/java/de/fhg/fokus/streetlife/mmecp/websocket/ToPanelEndpoint.java
@@ -1,12 +1,19 @@
 package de.fhg.fokus.streetlife.mmecp.websocket;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
 import java.util.LinkedList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
 
+import org.apache.commons.io.IOUtils;
+import org.glassfish.grizzly.utils.Charsets;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,43 +23,59 @@ import org.slf4j.LoggerFactory;
 @ServerEndpoint(value = "/objects")
 public class ToPanelEndpoint {
 
-    private final Logger LOG = LoggerFactory.getLogger(this.getClass());
-    private static List<Session> connectedSessions = new LinkedList<>();
+	private final Logger LOG = LoggerFactory.getLogger(this.getClass());
+	private static List<Session> connectedSessions = new LinkedList<>();
 
-    @OnOpen
-    public void onOpen(Session session) {
-        LOG.info("User {} connected...", session.getId());
-        connectedSessions.add(session);
+	@OnOpen
+	public void onOpen(Session session) {
+		LOG.info("User {} connected...", session.getId());
+		connectedSessions.add(session);
+        InputStream in = this.getClass().getResourceAsStream("/json/example1.json");
+        StringWriter writer = new StringWriter();
+
         try {
-            session.getBasicRemote().sendText("Welcome, this is your sessionId: " + session.getId());
+            IOUtils.copy(in, writer);
+			session.getBasicRemote().sendText("Welcome, this is your sessionId: " + session.getId());
+            session.getBasicRemote().sendText(writer.toString());
+            LOG.info("Sending JSON to session {} \n {}", session.getId(), writer.toString());
+		} catch (IOException e) {
+			LOG.error("onOpen sessionId {}", session.getId(), e);
+		}
+	}
+
+	@OnMessage
+	public void onMessage(String message, Session session) {
+		broadcastToAll("user " + session.getId() + " says > " + message);
+		LOG.info("User {} says: {}", session.getId(), message);
+	}
+
+	@OnClose
+	public void onClose(Session session, CloseReason closeReason) {
+		connectedSessions.remove(session);
+		broadcastToAll("user" + session.getId() + " closed connection...");
+		LOG.info("Connection to user {} closed...", session.getId());
+	}
+
+	@OnError
+	public void onError(Session session, Throwable t) {
+		LOG.error("Something went wrong in session [{}]", session.getId(), t);
+        LOG.error("Trying to close session {}", session.getId());
+        try {
+            session.close();
         } catch (IOException e) {
-            LOG.error("onOpen sessionId {}", session.getId(), e);
+            LOG.error("Can not close session [{}]", session.getId(), e);
         }
-    }
+	}
 
-    @OnMessage
-    public void onMessage(String message, Session session) {
-        broadcastToAll("user " + session.getId() + " says > " + message);
-        LOG.info("User {} says: {}", session.getId(), message);
-    }
-
-    @OnClose
-    public void onClose(Session session, CloseReason closeReason) {
-        connectedSessions.remove(session);
-        broadcastToAll("user" + session.getId() + " closed connection...");
-        LOG.info("Connection to user {} closed...", session.getId());
-    }
-
-
-    private void broadcastToAll(final String s) {
-        for (Session session : connectedSessions) {
-            try {
-                if (session.isOpen()) {
-                    session.getBasicRemote().sendText(s);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+	private void broadcastToAll(final String s) {
+		for (Session session : connectedSessions) {
+			try {
+				if (session.isOpen()) {
+					session.getBasicRemote().sendText(s);
+				}
+			} catch (IOException e) {
+				LOG.error("Can not send message to session [{}]", session.getId(), e);
+			}
+		}
+	}
 }
--- api-websocket/src/test/java/de/fhg/fokus/streetlife/mmecp/websocket/ToPanelEndpointTest.java
+++ api-websocket/src/test/java/de/fhg/fokus/streetlife/mmecp/websocket/ToPanelEndpointTest.java
@@ -12,9 +12,7 @@ import org.glassfish.tyrus.server.Server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 /**
  * Created by bdi on 03/11/14.
@@ -26,7 +24,7 @@ public class ToPanelEndpointTest {
 	private final Logger LOG = LoggerFactory.getLogger(this.getClass());
 	private Server server;
 
-	@BeforeClass
+	@BeforeTest
 	public void init() {
 		server = new Server("localhost", 8025, "/api-websocket", null, ToPanelEndpoint.class);
 		try {
@@ -69,14 +67,16 @@ public class ToPanelEndpointTest {
 	}
 
 	// This is just to show how to start the annoted client! Please use annotated method for further implementation.
-	// @Test
+	@Test
 	public void testToPanelEndpointAnnotated() throws IOException, DeploymentException {
-		WebSocketContainer container = ContainerProvider.getWebSocketContainer();
-		String uri = "ws://localhost:8080/api-websocket/objects";
-		container.connectToServer(ClientEndpoint.class, URI.create(uri));
+        if (true) {
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            String uri = "ws://localhost:8080/api-websocket/objects";
+            container.connectToServer(ClientEndpoint.class, URI.create(uri));
+        }
 	}
 
-	@AfterClass
+	@AfterTest
 	public void afterClass() {
 		server.stop();
 		LOG.info("Standalone websocket sever stopped...");

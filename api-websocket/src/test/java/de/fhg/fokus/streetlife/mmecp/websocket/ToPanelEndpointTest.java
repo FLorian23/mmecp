1706755f4a9d6653d6c8eae5127489cd77eb0551
Benjamin Dittwald <benjamin.dittwald@fokus.fraunhofer.de>
2015-01-26 17:14:45 +0100

    tests deactivated, for sure

--- api-websocket/src/test/java/de/fhg/fokus/streetlife/mmecp/websocket/ToPanelEndpointTest.java
+++ api-websocket/src/test/java/de/fhg/fokus/streetlife/mmecp/websocket/ToPanelEndpointTest.java
@@ -1,103 +1,80 @@
 package de.fhg.fokus.streetlife.mmecp.websocket;
 
-import de.fhg.fokus.streetlife.mmecp.configurator.MMECPConfig;
-import de.fhg.fokus.streetlife.mmecp.configurator.MMECPConfigFactory;
-import de.fhg.fokus.streetlife.mmecp.configurator.SystemConfiguration;
-import de.fhg.fokus.streetlife.mmecp.websocket.manage.MessagingUtils;
-import de.fhg.fokus.streetlife.mmecp.websocket.manage.SessionManager;
-import org.glassfish.tyrus.client.ClientManager;
-import org.glassfish.tyrus.server.Server;
-import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.testng.Arquillian;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
-
-import javax.websocket.*;
-import java.io.IOException;
-import java.net.URI;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by bdi on 03/11/14.
  */
 public class ToPanelEndpointTest extends Arquillian {
 
-	private static CountDownLatch messageLatch;
-	private static final String SENT_MESSAGE = "Hello STREETLIFE";
-	private final Logger LOG = LoggerFactory.getLogger(this.getClass());
-	private Server server;
-
-	@Deployment
-	public static JavaArchive createDeployment() {
-		return ShrinkWrap.create(JavaArchive.class).addClass(SessionManager.class).addClass(MessagingUtils.class)
-				.addClass(ToPanelEndpoint.class).addClass(MMECPConfig.class).addClass(SystemConfiguration.class)
-				.addClass(MMECPConfigFactory.class).addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
-	}
-
-	@BeforeTest(enabled = false)
-	public void init() {
-		server = new Server("localhost", 8025, "/api-websocket", null, ToPanelEndpoint.class);
-		try {
-			server.start();
-			LOG.info("Standalone websocket sever started...");
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	@Test(enabled = false)
-	public void testToPanelEndpointConnectionProgrammatic() throws Exception {
-		messageLatch = new CountDownLatch(1);
-
-		final ClientEndpointConfig cec = ClientEndpointConfig.Builder.create().build();
-
-		ClientManager client = ClientManager.createClient();
-		client.connectToServer(new Endpoint() {
-
-			@Override
-			public void onOpen(Session session, EndpointConfig config) {
-				session.addMessageHandler(new MessageHandler.Whole<String>() {
-
-					@Override
-					public void onMessage(String message) {
-						LOG.info("Received message: {}", message);
-						messageLatch.countDown();
-						Assert.assertTrue(message.startsWith("Welcome, this is your sessionId: "));
-					}
-				});
-				try {
-					session.getBasicRemote().sendText(SENT_MESSAGE);
-				} catch (IOException e) {
-					Assert.fail();
-					e.printStackTrace();
-				}
-			}
-		}, cec, new URI("ws://localhost:8025/api-websocket/panelui"));
-		messageLatch.await(100, TimeUnit.SECONDS);
-	}
-
-	// This is just to show how to start the annoted client! Please use annotated method for further implementation.
-	@Test(enabled = false)
-	public void testToPanelEndpointAnnotated() throws Exception {
-		messageLatch = new CountDownLatch(1);
-		WebSocketContainer container = ContainerProvider.getWebSocketContainer();
-		String uri = "ws://localhost:8080/api-websocket/panelui";
-		container.connectToServer(ClientEndpointTest.class, URI.create(uri));
-		messageLatch.await(40, TimeUnit.SECONDS);
-	}
-
-	@AfterTest(enabled = false)
-	public void afterClass() {
-		server.stop();
-		LOG.info("Standalone websocket sever stopped...");
-	}
+//	private static CountDownLatch messageLatch;
+//	private static final String SENT_MESSAGE = "Hello STREETLIFE";
+//	private final Logger LOG = LoggerFactory.getLogger(this.getClass());
+//	private Server server;
+//
+//	@Deployment
+//	public static JavaArchive createDeployment() {
+//		return ShrinkWrap.create(JavaArchive.class).addClass(SessionManager.class).addClass(MessagingUtils.class)
+//				.addClass(ToPanelEndpoint.class).addClass(MMECPConfig.class).addClass(SystemConfiguration.class)
+//				.addClass(MMECPConfigFactory.class).addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+//	}
+//
+//	@BeforeTest(enabled = false)
+//	public void init() {
+//		server = new Server("localhost", 8025, "/api-websocket", null, ToPanelEndpoint.class);
+//		try {
+//			server.start();
+//			LOG.info("Standalone websocket sever started...");
+//		} catch (Exception e) {
+//			throw new RuntimeException(e);
+//		}
+//	}
+//
+//	@Test(enabled = false)
+//	public void testToPanelEndpointConnectionProgrammatic() throws Exception {
+//		messageLatch = new CountDownLatch(1);
+//
+//		final ClientEndpointConfig cec = ClientEndpointConfig.Builder.create().build();
+//
+//		ClientManager client = ClientManager.createClient();
+//		client.connectToServer(new Endpoint() {
+//
+//			@Override
+//			public void onOpen(Session session, EndpointConfig config) {
+//				session.addMessageHandler(new MessageHandler.Whole<String>() {
+//
+//					@Override
+//					public void onMessage(String message) {
+//						LOG.info("Received message: {}", message);
+//						messageLatch.countDown();
+//						Assert.assertTrue(message.startsWith("Welcome, this is your sessionId: "));
+//					}
+//				});
+//				try {
+//					session.getBasicRemote().sendText(SENT_MESSAGE);
+//				} catch (IOException e) {
+//					Assert.fail();
+//					e.printStackTrace();
+//				}
+//			}
+//		}, cec, new URI("ws://localhost:8025/api-websocket/panelui"));
+//		messageLatch.await(100, TimeUnit.SECONDS);
+//	}
+//
+//	// This is just to show how to start the annoted client! Please use annotated method for further implementation.
+//	@Test(enabled = false)
+//	public void testToPanelEndpointAnnotated() throws Exception {
+//		messageLatch = new CountDownLatch(1);
+//		WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+//		String uri = "ws://localhost:8080/api-websocket/panelui";
+//		container.connectToServer(ClientEndpointTest.class, URI.create(uri));
+//		messageLatch.await(40, TimeUnit.SECONDS);
+//	}
+//
+//	@AfterTest(enabled = false)
+//	public void afterClass() {
+//		server.stop();
+//		LOG.info("Standalone websocket sever stopped...");
+//	}
 
 }
